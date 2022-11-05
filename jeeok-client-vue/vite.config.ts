2b6a992e1490c08a27c918c7cb49b3caf8fbe5e0
hclee <hclee@dataeum.kr>
2022-11-05 20:12:51 +0900

    cors 추가

--- jeeok-client-vue/vite.config.ts
+++ jeeok-client-vue/vite.config.ts
@@ -12,4 +12,9 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      "/api": "http://localhost:11000",
+    },
+  },
 });
