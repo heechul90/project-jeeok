52ffb692a5ed3a04f9332c5b9f91dd54ee0e03ed
hclee <hclee@dataeum.kr>
2022-11-16 15:31:47 +0900

    jeeokshop > order-server > common 추가

---
+++ jeeokshop-order-server/src/main/java/com/jeeok/jeeokshop/common/entity/BaseTimeEntity.java
@@ -0,0 +1,26 @@
+package com.jeeok.jeeokshop.common.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public abstract class BaseTimeEntity {
+
+    /** 등록시점 */
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    /** 수정시점 */
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+}
---
+++ jeeokshop-order-server/src/main/java/com/jeeok/jeeokshop/common/json/JsonResult.java
@@ -0,0 +1,55 @@
+package com.jeeok.jeeokshop.common.json;
+
+import lombok.*;
+import org.springframework.lang.Nullable;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class JsonResult<T> {
+
+    private LocalDateTime transaction_time;
+    private Code code;
+    private String message;
+
+    private List<JsonError> errors;
+
+    @Nullable
+    private T data;
+
+    public static <T> JsonResult<T> OK() {
+        return (JsonResult<T>) JsonResult.builder()
+                .transaction_time(LocalDateTime.now())
+                .code(Code.SUCCESS)
+                .build();
+    }
+
+    public static <T> JsonResult<T> OK(T data) {
+        return (JsonResult<T>) JsonResult.builder()
+                .transaction_time(LocalDateTime.now())
+                .code(Code.SUCCESS)
+                .data(data)
+                .build();
+    }
+
+    public static <T> JsonResult<T> ERROR(String message) {
+        return (JsonResult<T>) JsonResult.builder()
+                .transaction_time(LocalDateTime.now())
+                .code(Code.ERROR)
+                .message(message)
+                .build();
+    }
+
+    public static <T> JsonResult<T> ERROR(String message, List<JsonError> errors) {
+        return (JsonResult<T>) JsonResult.builder()
+                .transaction_time(LocalDateTime.now())
+                .code(Code.ERROR)
+                .message(message)
+                .errors(errors)
+                .build();
+    }
+}
