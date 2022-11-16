fc776550a83c14296ca671e3b97b20653dfd6899
hclee <hclee@dataeum.kr>
2022-11-16 15:07:56 +0900

    jeeokshop order-server 추가

---
+++ jeeokshop-order-server/src/main/java/com/jeeok/jeeokshop/JeeokshopOrderServerApplication.java
@@ -0,0 +1,13 @@
+package com.jeeok.jeeokshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeokshopOrderServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeeokshopOrderServerApplication.class, args);
+    }
+
+}
