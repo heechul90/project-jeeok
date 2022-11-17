cc38d1692295b7c2d1463bc3b199980397112bd6
hclee <hclee@dataeum.kr>
2022-11-17 11:28:26 +0900

    jeeokshop > delivery-server > config 추가

---
+++ jeeokshop-delivery-server/src/main/java/com/jeeok/jeeokshop/config/AuditConfig.java
@@ -0,0 +1,19 @@
+package com.jeeok.jeeokshop.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@Slf4j
+@Configuration
+@EnableJpaAuditing
+public class AuditConfig implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.empty();
+    }
+}
