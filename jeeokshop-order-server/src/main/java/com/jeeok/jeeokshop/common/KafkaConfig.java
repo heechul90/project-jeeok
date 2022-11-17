e00334c07ea4ba87647be05d7db973bdfe83339b
hclee <hclee@dataeum.kr>
2022-11-17 14:57:48 +0900

    jeeokshop > order-server > order > kafka 설정 추가

---
+++ jeeokshop-order-server/src/main/java/com/jeeok/jeeokshop/common/KafkaConfig.java
@@ -0,0 +1,46 @@
+package com.jeeok.jeeokshop.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+
+    private final String kafkaServerHost;
+    private final String kafkaServerPort;
+
+    public KafkaConfig(@Value("${kafka.host}") String kafkaServerHost,
+                       @Value("${kafka.port}") String kafkaServerPort) {
+        this.kafkaServerHost = kafkaServerHost;
+        this.kafkaServerPort = kafkaServerPort;
+    }
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServerHost + ":" + kafkaServerPort);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, 5000);
+
+        return new DefaultKafkaProducerFactory<>(properties);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
