1ae3eb7bf9b9844c34df845fc48273a7af6ac970
hclee <hclee@dataeum.kr>
2022-11-17 10:59:38 +0900

    jeeokshop > delivery-server 추가

---
+++ jeeokshop-delivery-server/src/test/java/com/jeeok/jeeokshop/deliveryserver/JeeokshopDeliveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeeok.jeeokshop.deliveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeokshopDeliveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
