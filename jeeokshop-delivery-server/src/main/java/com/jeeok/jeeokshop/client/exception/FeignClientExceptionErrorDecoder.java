0bc7ceea6db173bd25ef74aa1bb3afefcfd9acfd
hclee <hclee@dataeum.kr>
2022-11-19 17:00:18 +0900

    jeeokshop > delivery-server > feign exception 추가

---
+++ jeeokshop-delivery-server/src/main/java/com/jeeok/jeeokshop/client/exception/FeignClientExceptionErrorDecoder.java
@@ -0,0 +1,36 @@
+package com.jeeok.jeeokshop.client.exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jeeok.jeeokshop.common.json.JsonResult;
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class FeignClientExceptionErrorDecoder implements ErrorDecoder {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        String message = null;
+        if (response.body() != null) {
+            try {
+                JsonResult jsonResult = objectMapper.readValue(response.body().asInputStream(), JsonResult.class);
+                message = jsonResult.getMessage();
+            } catch (IOException e) {
+                String catchErrorMessage = "Error Deserializing response body from failed feign request response.";
+                log.warn(methodKey + catchErrorMessage, e);
+                return new FeignClientException("고객센터로 문의해주세요.");
+            }
+        }
+
+        return new FeignClientException(message);
+    }
+}
