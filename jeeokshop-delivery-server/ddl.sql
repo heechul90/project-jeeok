c5011834320499e1dc450facb564867b2795168c
hclee <hclee@dataeum.kr>
2022-11-21 22:41:35 +0900

    delivery-server > update README.md

--- jeeokshop-delivery-server/ddl.sql
+++ jeeokshop-delivery-server/ddl.sql
@@ -14,3 +14,22 @@ create table delivery
     delivery_status varchar(15) null comment '배송 상태'
 );
 
+-- auto-generated definition
+create table delivery_rider
+(
+    delivery_rider_id   bigint auto_increment comment '배송 라이더 고유번호'
+        primary key,
+    created_date        datetime(6)  null comment '생성일',
+    last_modified_date  datetime(6)  null comment '수정일',
+    created_by          bigint       null comment '생생자 고유번호',
+    last_modified_by    bigint       null comment '수정자 고유번호',
+    phone_number_first  varchar(3)   null comment '휴대폰번호 첫자리',
+    phone_number_last   varchar(4)   null comment '휴대폰번호 중간자리',
+    phone_number_middle varchar(4)   null comment '휴대폰번호 마지막자리',
+    rider_id            bigint       null comment '라이더 고유번호',
+    rider_name          varchar(255) null comment '라이더 이름',
+    delivery_id         bigint       null comment '배송 고유번호',
+    constraint FK8h32gnlrewv93wfwvd8g4merr
+        foreign key (delivery_id) references delivery (delivery_id)
+);
+
