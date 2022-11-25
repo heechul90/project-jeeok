e51c147f1bca60287118870bb412b6308707f6ab
hclee <hclee@dataeum.kr>
2022-11-25 21:00:11 +0900

    jeeoklog-server > sleuth and zipkin 추가

--- jeeoklog-server/src/main/java/com/jeeok/jeeoklog/config/audit/AuditConfig.java
+++ jeeoklog-server/src/main/java/com/jeeok/jeeoklog/config/audit/AuditConfig.java
@@ -1,4 +1,4 @@
-package com.jeeok.jeeoklog.config;
+package com.jeeok.jeeoklog.config.audit;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
---
+++ jeeoklog-server/src/main/java/com/jeeok/jeeoklog/config/p6spy/P6SpyConfiguration.java
@@ -0,0 +1,14 @@
+package com.jeeok.jeeoklog.config.p6spy;
+
+import com.p6spy.engine.spy.P6SpyOptions;
+
+import javax.annotation.PostConstruct;
+
+//@Configuration
+public class P6SpyConfiguration {
+
+    @PostConstruct
+    public void setMessageFormat() {
+        P6SpyOptions.getActiveInstance().setLogMessageFormat(CustomP6SpySqlFormat.class.getName());
+    }
+}
