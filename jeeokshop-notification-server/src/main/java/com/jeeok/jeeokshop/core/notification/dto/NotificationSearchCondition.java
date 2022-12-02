3e613f9b2df5d44ec7b7d8f408e98c3287db47e0
hclee <hclee@dataeum.kr>
2022-12-02 19:43:41 +0900

    notification-server > core > repository 추가

--- jeeokshop-notification-server/src/main/java/com/jeeok/jeeokshop/core/notification/dto/NotificationSearchCondition.java
+++ jeeokshop-notification-server/src/main/java/com/jeeok/jeeokshop/core/notification/dto/NotificationSearchCondition.java
@@ -6,4 +6,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class NotificationSearchCondition {
+
+    private Long searchMemberId;
 }
--- jeeokshop-notification-server/src/main/java/com/jeeok/jeeokshop/core/notification/repository/NotificationQueryRepository.java
+++ jeeokshop-notification-server/src/main/java/com/jeeok/jeeokshop/core/notification/repository/NotificationQueryRepository.java
@@ -2,12 +2,18 @@ package com.jeeok.jeeokshop.core.notification.repository;
 
 import com.jeeok.jeeokshop.core.notification.domain.Notification;
 import com.jeeok.jeeokshop.core.notification.dto.NotificationSearchCondition;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
+
+import static com.jeeok.jeeokshop.core.notification.domain.QNotification.notification;
 
 @Repository
 public class NotificationQueryRepository {
@@ -18,8 +24,50 @@ public class NotificationQueryRepository {
         this.queryFactory = new JPAQueryFactory(em);
     }
 
+    /**
+     * 알람 목록 조회
+     */
     public Page<Notification> findNotifications(NotificationSearchCondition condition, Pageable pageable) {
 
-        return null;
+        List<Notification> content = getNotificationList(condition, pageable);
+
+        JPAQuery<Long> count = getNotificationListCount(condition);
+
+        return PageableExecutionUtils.getPage(content, pageable, count::fetchOne);
+    }
+
+    /**
+     * 알람 목록
+     */
+    private List<Notification> getNotificationList(NotificationSearchCondition condition, Pageable pageable) {
+        return queryFactory
+                .select(notification)
+                .from(notification)
+                .where(
+                        searchMemberIdEq(condition.getSearchMemberId())
+                )
+                .orderBy(notification.memberId.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+
+    /**
+     * 알람 목록 카운트
+     */
+    private JPAQuery<Long> getNotificationListCount(NotificationSearchCondition condition) {
+        return queryFactory
+                .select(notification.count())
+                .from(notification)
+                .where(
+                        searchMemberIdEq(condition.getSearchMemberId())
+                );
+    }
+
+    /**
+     * where memberId == searchMemberId
+     */
+    private BooleanExpression searchMemberIdEq(Long searchMemberId) {
+        return searchMemberId != null ? notification.memberId.eq(searchMemberId) : null;
     }
 }
