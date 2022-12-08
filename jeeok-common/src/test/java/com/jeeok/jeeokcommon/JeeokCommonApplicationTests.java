7526a5684203ee280dbc667bd878856237925fb0
hclee <hclee@dataeum.kr>
2022-12-08 19:26:38 +0900

    jeeok-common 추가

---
+++ jeeok-common/src/test/java/com/jeeok/jeeokcommon/JeeokCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeeok.jeeokcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeokCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
