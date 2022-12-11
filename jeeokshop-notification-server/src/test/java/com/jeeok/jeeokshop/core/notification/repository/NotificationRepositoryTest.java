c427d385ab9c65c9fc71ab581ac09a86ff6bf95f
hclee <heechul4296@gmail.com>
2022-12-11 18:27:35 +0900

    jeeokshop-notificaion-server > repository > test 추가

--- jeeokshop-notification-server/src/test/java/com/jeeok/jeeokshop/core/notification/repository/NotificationRepositoryTest.java
+++ jeeokshop-notification-server/src/test/java/com/jeeok/jeeokshop/core/notification/repository/NotificationRepositoryTest.java
@@ -1,10 +1,11 @@
 package com.jeeok.jeeokshop.core.notification.repository;
 
+import com.jeeok.jeeokshop.core.RepositoryTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class NotificationRepositoryTest {
+class NotificationRepositoryTest extends RepositoryTest {
 
     @Test
     void findNotifications() {
